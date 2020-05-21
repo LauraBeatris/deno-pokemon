d0495debdb3ed0cee8afdb00810797a5be36e186
Laura <laurabeatriserafim@gmail.com>
2020-05-21 09:18:31 -0300

    refactor: change app filename

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 // Allow access network, enviroment variables and file system read
-deno run --allow-net --allow-env --allow-read src/app.ts
+deno run --allow-net --allow-env --allow-read src/index.ts
