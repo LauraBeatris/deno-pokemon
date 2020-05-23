67c4f04bad7854d0dcd3565d1969dd66ea70e481
Edilson Alves <email@edilsonalves.com>
2020-05-23 17:22:43 -0300

    refactor: Change Dotenv settings

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,10 @@
-const env = Deno.env.toObject();
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-export const APP_HOST = env.APP_HOST || "127.0.0.1";
-export const APP_PORT = env.APP_PORT || 3333;
+config({ export: true });
+
+const env = {
+  APP_HOST: Deno.env.get("APP_HOST") || "127.0.0.1",
+  APP_PORT: Deno.env.get("APP_PORT") || 3333,
+};
+
+export default env;
